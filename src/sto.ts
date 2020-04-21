a85d8b63689efa9e922e8ae3a5db999bc896ae57
MamoruDS <mamoruds.io@gmail.com>
2020-04-22 08:45:48 +0900

    reworte methods in sto.ts with classes
    improved add method getSTO for getting global STO instance

--- src/sto.ts
+++ src/sto.ts
@@ -1,4 +1,3 @@
-import * as fs from 'fs'
 import * as path from 'path'
 
 import * as lowdb from 'lowdb'
@@ -6,24 +5,11 @@ import * as FileSync from 'lowdb/adapters/FileSync'
 import * as _ from 'lodash'
 
 import * as utils from './utils'
-import { encrypt, decrypt } from './encrypt'
+import { encrypt } from './encrypt'
 import { options as OPT } from './index'
 
-const CACHE_DIR = './cache'
+const CACHE_DIR = '.'
 const CACHE_PATH = path.join(CACHE_DIR, 'cache.json')
-
-if (!fs.existsSync(CACHE_DIR)) {
-    fs.mkdirSync(CACHE_DIR)
-}
-
-if (!fs.statSync(CACHE_DIR).isDirectory()) {
-    process.exit(1)
-}
-
-const adapter = new FileSync(CACHE_PATH)
-const db = lowdb(adapter)
-
-db.defaults({ USER: {} }).write()
 
 const parseDBPath = (path: string[]) => {
     return path
@@ -34,17 +20,6 @@ const parseDBPath = (path: string[]) => {
             return p
         })
         .join('.')
-}
-
-const get = (path: string[]) => {
-    return db.get(parseDBPath(path))
-}
-
-const set = (path: string[], data?: any) => {
-    if (typeof data === 'undefined') {
-        return db.unset(parseDBPath(path))
-    }
-    return db.set(parseDBPath(path), data)
 }
 
 interface CacheData {
@@ -61,90 +36,142 @@ export type RequestInfo = {
     }
 }
 
-const getUsers = (): string[] => {
-    const _user = get(['USER']).value()
-    if (_user === null) {
+class Users {
+    private _sto: STO
+    constructor(sto: STO) {
+        this._sto = sto
+    }
+
+    get list(): string[] {
+        const _user = this._sto.get(['USER']).value()
+        if (_user === null) {
+            return []
+        }
+        return Object.keys(_user)
+    }
+    get length(): number {
+        return this.list.length
+    }
+
+    get(id: string): User {
+        return new User(id, this._sto)
+    }
+    add(alias: string): string {
+        const id = utils.genId('USER')
+        this._sto.set(['USER', id, 'ALIAS'], alias).write()
+        return id
+    }
+    delete(id: string) {
+        this._sto.set([id]).write()
+    }
+}
+
+class User {
+    public id: string
+    private _sto: STO
+
+    constructor(id: string, sto: STO) {
+        this.id = id
+        this._sto = sto
+    }
+
+    get pending(): number[] {
+        const p = this._sto.get(['USER', this.id, 'DATA']).value()
+        if (p) {
+            const ids = []
+            for (let id of Object.keys(p)) {
+                const regex = new RegExp(/_(\d{1,})/, 'g')
+                const res = regex.exec(id)
+                if (res !== null) {
+                    ids.push(parseInt(res[1]))
+                } else {
+                    ids.push(id)
+                }
+            }
+            return ids
+        }
         return []
     }
-    return Object.keys(_user)
+
+    getCacheData(pendingId: number): CacheData {
+        try {
+            return JSON.parse(
+                this._sto.get(['USER', this.id, 'DATA', `${pendingId}`]).value()
+            ) as CacheData
+        } catch (err) {
+            if (err instanceof SyntaxError) return undefined
+        }
+    }
+    setCacheData(pendingId: number, data?: CacheData): void {
+        this._sto
+            .set(
+                ['USER', this.id, 'DATA', `${pendingId}`],
+                JSON.stringify(data)
+            )
+            .write()
+    }
+    delCacheData(pendingId: number): void {
+        this._sto.set(['USER', this.id, 'DATA', `${pendingId}`]).write()
+    }
 }
 
-export const createUser = (alias: string): string => {
-    const id = utils.genId('USER')
-    set(['USER', id, 'ALIAS'], alias).write()
-    return id
-}
+class STO {
+    private _db
+    private _users: Users
 
-export const getUserPending = (user: string): number[] => {
-    const p = get(['USER', user, 'DATA']).value()
-    if (p) {
-        const ids = []
-        for (let id of Object.keys(p)) {
-            const regex = new RegExp(/_(\d{1,})/, 'g')
-            const res = regex.exec(id)
-            if (res !== null) {
-                ids.push(parseInt(res[1]))
-            } else {
-                ids.push(id)
+    constructor() {
+        const adapter = new FileSync(CACHE_PATH)
+        this._db = lowdb(adapter)
+        this._db.defaults({ USER: {} }).write()
+        this._users = new Users(this)
+    }
+
+    get user(): Users {
+        return this._users
+    }
+    get lastPendingId(): number {
+        const users = this._users.list
+        let latestId = 0
+        for (const userId of users) {
+            const user = new User(userId, this)
+            const pendingIds = user.pending
+            for (const id of pendingIds) {
+                latestId = id > latestId ? id : latestId
             }
         }
-        return ids
+        return latestId
     }
-    return []
-}
 
-export const removeUser = (user: string) => {
-    set([user]).write()
-}
-
-const getCacheData = (user: string, id: number): CacheData => {
-    try {
-        return JSON.parse(
-            get(['USER', user, 'DATA', `${id}`]).value()
-        ) as CacheData
-    } catch (err) {
-        if (err instanceof SyntaxError) return undefined
+    get(path: string[]) {
+        return this._db.get(parseDBPath(path))
     }
-}
-
-const setCacheData = (user: string, id: number, data?: CacheData): void => {
-    set(['USER', user, 'DATA', `${id}`], JSON.stringify(data)).write()
-}
-
-export const removeCacheData = (user: string, id: number) => {
-    set(['USER', user, 'DATA', `${id}`]).write()
-}
-
-export const getLatestPendingId = (): number => {
-    const users = getUsers()
-    let latestId = 0
-    for (const user of users) {
-        const pendingIds = getUserPending(user)
-        for (const id of pendingIds) {
-            latestId = id > latestId ? id : latestId
+    set(path: string[], data?: any) {
+        if (typeof data === 'undefined') {
+            return this._db.unset(parseDBPath(path))
         }
+        return this._db.set(parseDBPath(path), data)
     }
-    return latestId
 }
 
 export class CachedData {
-    private readonly _user: string
+    private readonly _userId: string
     private readonly _pendingId: number
     private readonly _requestInfo: RequestInfo
     private readonly _verifyKey: string
 
-    constructor(user: string, pendindId?: number, reqInfo?: RequestInfo) {
-        this._user = user
+    constructor(userId: string, pendindId?: number, reqInfo?: RequestInfo) {
+        this._userId = userId
         this._pendingId = pendindId || OPT.pendingId
         OPT.pendingId++
-        const _data = getCacheData(user, this._pendingId)
+        const sto = getSTO()
+        const _data = sto.user.get(this._userId).getCacheData(this._pendingId)
         if (_data) {
             this._requestInfo = _data.request
             this._verifyKey = _data.verify_token
         } else {
             this._requestInfo = reqInfo
             this._verifyKey = utils.genRandom(6).toUpperCase()
-            setCacheData(this._user, this._pendingId, {
+            sto.user.get(this._userId).setCacheData(this._pendingId, {
                 verify_token: this._verifyKey,
                 request: this._requestInfo,
             })
@@ -171,11 +198,14 @@ export class CachedData {
         return await encrypt(JSON.stringify(this._cacheData), OPT.password)
     }
     clean() {
-        setCacheData(this._user, this._pendingId)
+        sto.user.get(this._userId).delCacheData(this._pendingId)
     }
 }
 
-export const dataDecrypt = async (encrypted: string): Promise<CacheData> => {
-    const _str = await decrypt(encrypted, OPT.password)
-    return JSON.parse(_str) as CacheData
+let sto: STO = undefined
+export const getSTO = (): STO => {
+    if (typeof sto === 'undefined') {
+        sto = new STO()
+    }
+    return sto
 }
