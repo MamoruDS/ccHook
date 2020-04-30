498474b548bb504048e01446e63d7a3a3ca7e935
MamoruDS <mamoruds.io@gmail.com>
2020-04-30 17:35:47 +0900

    fixed missing offset of last pending id
    improved using router in server.ts and export it that server mode now can using as module
    
    improved moving password generation and pendingId calculation from index.ts to server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,80 +1,114 @@
 import * as express from 'express'
 import * as bodyParser from 'body-parser'
 
-import { getSTO, CachedData } from './sto'
+import { STO, getSTO, CachedData } from './sto'
 import { options as OPT } from './index'
+import { genRandom, genRandomNum } from './utils'
 
-export const server = () => {
-    const sto = getSTO()
-    const app = express()
+export const router = express.Router()
 
-    app.use(bodyParser.json())
+let sto = undefined as STO
 
-    app.get('/health', (req, res) => {
-        res.status(200).send()
+router.route('/').get((req, res) => {
+    res.status(202).json({
+        err: false,
+        info: 'CCHOOK API',
+    })
+})
+
+router.route('/cchook').post(async (req, res) => {
+    const _headers = {}
+    Object.keys(req.headers).map((key) => {
+        _headers[key] = req.headers[key]
+    })
+    const _body = {}
+    Object.keys(req.body).map((key) => {
+        _body[key] = req.body[key]
+    })
+    const userId = _body['_cc_hook_user_id']
+    if (!userId) {
+        res.status(400).json({
+            err: true,
+            errMsg: 'missing user id in request',
+        })
         return
+    } else if (sto.user.list.lastIndexOf(userId) == -1) {
+        res.status(400).json({
+            err: true,
+            errMsg: 'user not found',
+        })
+        return
+    }
+    delete _body['_cc_hook_user_id']
+    const _data = new CachedData(userId, undefined, {
+        body: _body,
+        headers: _headers,
     })
-
-    app.post('/cchook', async (req, res) => {
-        const _headers = {}
-        Object.keys(req.headers).map((key) => {
-            _headers[key] = req.headers[key]
-        })
-        const _body = {}
-        Object.keys(req.body).map((key) => {
-            _body[key] = req.body[key]
-        })
-        const userId = _body['_cc_hook_user_id']
-        if (!userId) {
-            res.status(400).end()
-            return
-        }
-        delete _body['_cc_hook_user_id']
-        const _data = new CachedData(userId, undefined, {
-            body: _body,
-            headers: _headers,
-        })
-        res.status(200).json({
-            pending_id: _data.pendingId,
-        })
+    res.status(200).json({
+        err: false,
+        pending_id: _data.pendingId,
     })
+})
 
-    app.post('/pending', async (req, res) => {
-        const userId = req.body['_cc_hook_user_id']
-        const offset = req.body['_cc_hook_offset'] || -1
-        if (!userId) {
-            res.status(400).end()
-            return
-        }
-        const pendingIds = sto.user.get(userId).pending
-        const data = []
-        for (const id of pendingIds) {
-            if (id <= offset) {
-                sto.user.get(userId).delCacheData(id)
-                continue
-            }
-            const cachedData = new CachedData(userId, id)
-            data.push({
-                pending_id: cachedData.pendingId,
-                data: await cachedData.encrypt(),
-            })
-        }
-        res.status(200).json({
-            datas: data,
+router.route('/pending').post(async (req, res) => {
+    const userId = req.body['_cc_hook_user_id']
+    const offset = req.body['_cc_hook_offset'] || -1
+    if (!userId) {
+        res.status(400).json({
+            err: true,
+            errMsg: 'missing user id in request',
         })
+        return
+    }
+    const pendingIds = sto.user.get(userId).pending
+    const data = []
+    for (const id of pendingIds) {
+        if (id <= offset) {
+            sto.user.get(userId).delCacheData(id)
+            continue
+        }
+        const cachedData = new CachedData(userId, id)
+        data.push({
+            pending_id: cachedData.pendingId,
+            data: await cachedData.encrypt(),
+        })
+    }
+    res.status(200).json({
+        err: false,
+        datas: data,
     })
+})
 
-    app.post('/user', async (req, res) => {
+router
+    .route('/user')
+    .post(async (req, res) => {
         const alias = req.body['alias']
         if (!alias) {
             res.status(400).end()
             return
         }
         const id = sto.user.add(alias)
-        res.json({
+        res.status(200).json({
+            err: false,
             user_id: id,
         })
-    }).delete('/user', async (req, res) => {})
+    })
+    .delete(async (req, res) => {})
+
+export const server = () => {
+    sto = getSTO()
+    const latestId = sto.lastPendingId
+    OPT.pendingId = (latestId !== 0 ? latestId : genRandomNum(10000, 50000)) + 1
+    if (!OPT.password) {
+        const passwd = genRandom(8).toUpperCase()
+        OPT.password = passwd
+        console.log(`Generate new password: ${passwd}`)
+    }
+    const app = express()
+
+    app.use(bodyParser.json())
+
+    app.use('/', router)
 
     app.listen(OPT.port, () => {
         console.log('CC-HOOK START ON:', OPT.port)
